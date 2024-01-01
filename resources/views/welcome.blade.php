fa3faca772ed3214d2a2ffbf7e031294b44e61a0
jbiddulph <john.mbiddulph@gmail.com>
2024-01-01 22:59:10 +0000

    Naked sloth logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,36 +27,9 @@
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Raleway:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="css/grayscale.min.css" rel="stylesheet">
-
 </head>
 
 <body id="page-top">
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/nakedsloth-logo@2x.png" height="65" alt="Naked Sloth Web Development"></a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#projects">Process</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
 
   <!-- Header -->
   <header class="masthead">
@@ -69,119 +42,9 @@
     </div>
   </header>
 
-  <!-- About Section -->
-  <section id="about" class="about-section text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 mx-auto">
-          <h2 class="text-white mb-4">Built with YOU in mind</h2>
-          <p class="text-white-50">YOU come first, YOU know what YOU want, WE at Naked Sloth, design, build and develop with YOU in mind.
-            <a href="mailto:hello@nakedsloth.co.uk">Email us now for a quote</a>.
-            Weather your project is small, medium or large, Naked Sloth Design and Development team can<br /> Design, Build and Develop for YOU</p>
-        </div>
-      </div>
-      <img src="img/ipad.png" class="img-fluid" alt="">
-    </div>
-  </section>
-
-  <!-- Projects Section -->
-  <section id="projects" class="projects-section bg-light">
-    <div class="container">
-
-      <!-- Featured Project Row -->
-      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-        <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="featured-text text-center text-lg-left">
-            <h4>The Sloth</h4>
-            <p class="text-black-50 mb-0">The animals live solitary lives and travel from tree to tree using canopy vines. Located in places such as Brazil and Panama, the six species of this strange and wonderful animal need healthy forests to survive.</p>
-          </div>
-        </div>
-      </div>
-
-      <!-- Project One Row -->
-      <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
-        <div class="col-lg-6">
-          <img class="img-fluid" src="img/design-wireframe.jpg" alt="Wireframe design stage">
-        </div>
-        <div class="col-lg-6">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-left">
-                <h4 class="text-white">Design</h4>
-                <p class="mb-0 text-white-50">Wireframing and design for all different device types and screen sizes. There will be a lot of communication between us and you during this stage.</p>
-                <hr class="d-none d-lg-block mb-0 ml-0">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Project Two Row -->
-      <div class="row justify-content-center no-gutters">
-        <div class="col-lg-6">
-          <img class="img-fluid" src="img/coding.jpg" alt="Laravel HTML code">
-        </div>
-        <div class="col-lg-6 order-lg-first">
-          <div class="bg-black text-center h-100 project">
-            <div class="d-flex h-100">
-              <div class="project-text w-100 my-auto text-center text-lg-right">
-                <h4 class="text-white">Build</h4>
-                <p class="mb-0 text-white-50">
-                  We build responsive (mobile friendly) websites. After the design process, the website will be built. We build (code) websites so they work across all types of devices and screen sizes. During the build stage, we will require the content from you.
-                </p>
-                <hr class="d-none d-lg-block mb-0 mr-0">
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  </section>
-
-  <!-- Signup Section -->
-  <section id="signup" class="signup-section">
-    <div class="container">
-<!--      <form action="mail.php" method="post" name="quote_form">-->
-<!--        <div class="row">-->
-<!--          <div class="col-md-10 col-lg-8 mx-auto text-center">-->
-
-<!--            <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>-->
-<!--            <h2 class="text-white mb-5">Request a quote now!</h2>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--        <div class="row">-->
-<!--          <div class="col-md-4 mx-auto text-center">-->
-<!--              <select name="require" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0">-->
-<!--                <option value="">Please select</option>-->
-<!--                <option value="new website">A new website</option>-->
-<!--                <option value="makeover">A complete make-over</option>-->
-<!--                <option value="redesign">A redesign</option>-->
-<!--                <option value="Business">Business website</option>-->
-<!--                <option value="Personal">Personal website</option>-->
-<!--                <option value="SEO">SEO (Search Engine Optimized)</option>-->
-<!--                <option value="Other">Other</option>-->
-<!--              </select>-->
-<!--          </div>-->
-<!--          <div class="col-md-4 mx-auto text-center">-->
-<!--            <input type="tel" name="telephone" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputTelephone" placeholder="Enter telephone...">-->
-<!--          </div>-->
-<!--          <div class="col-md-4 mx-auto text-center">-->
-<!--              <input type="email" name="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">-->
-<!--          </div>-->
-<!--        </div>-->
-        <div class="row">
-          <div class="col-md-8 col-lg-6 mx-auto text-center mt-4">
-            <a href="mailto:hello@nakedsloth.co.uk"><button class="btn btn-primary mx-auto quote">Request a quote now</button></a>
-          </div>
-        </div>
-<!--      </form>-->
-    </div>
-  </section>
-
+  <div>
+    <img src="./images/ns-logo.jpg" alt="Naked Sloth">
+  </div>
   <!-- Contact Section -->
   <section class="contact-section bg-black">
     <div class="container">
@@ -242,7 +105,7 @@
   <!-- Footer -->
   <footer class="bg-black small text-center text-white-50">
     <div class="container">
-      Copyright &copy; Naked Sloth 2020
+      Copyright &copy; Naked Sloth 2024
     </div>
   </footer>
 
